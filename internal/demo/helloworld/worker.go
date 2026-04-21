7202fc455c83780846f14dbb0548864e158b064a
shashwatsuri <shashwatsuri98@gmail.com>
2026-04-21 14:57:42 -0700

    Rainbow demo v118: sleep=270s

--- internal/demo/helloworld/worker.go
+++ internal/demo/helloworld/worker.go
@@ -25,13 +25,13 @@ func HelloWorld(ctx workflow.Context) (string, error) {
 	}
 
 
-	// Non-replay-safe change introduced by Rainbow demo v117
-	if err := workflow.Sleep(ctx, 240*time.Second); err != nil {
+	// Non-replay-safe change introduced by Rainbow demo v118
+	if err := workflow.Sleep(ctx, 270*time.Second); err != nil {
 		return "", err
 	}
 
 	// Return the greeting
-	return fmt.Sprintf("Hello %s (rainbow demo v117, sleep=240s)", subject), nil
+	return fmt.Sprintf("Hello %s (rainbow demo v118, sleep=270s)", subject), nil
 }
 
 func GetSubject(ctx context.Context) (string, error) {
