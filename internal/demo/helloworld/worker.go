ffaeb18c5bba71f97bf7f3ac771384b0603d3c86
shashwatsuri <shashwatsuri98@gmail.com>
2026-04-26 03:15:58 -0700

    Rainbow demo v501: sleep=201s

--- internal/demo/helloworld/worker.go
+++ internal/demo/helloworld/worker.go
@@ -25,13 +25,13 @@ func HelloWorld(ctx workflow.Context) (string, error) {
 	}
 
 
-	// Non-replay-safe change introduced by Rainbow demo v500
-	if err := workflow.Sleep(ctx, 190*time.Second); err != nil {
+	// Non-replay-safe change introduced by Rainbow demo v501
+	if err := workflow.Sleep(ctx, 201*time.Second); err != nil {
 		return "", err
 	}
 
 	// Return the greeting
-	return fmt.Sprintf("Hello %s (rainbow demo v500, sleep=190s)", subject), nil
+	return fmt.Sprintf("Hello %s (rainbow demo v501, sleep=201s)", subject), nil
 }
 
 func GetSubject(ctx context.Context) (string, error) {
@@ -44,7 +44,7 @@ func GetSubject(ctx context.Context) (string, error) {
 func RolloutGate(ctx workflow.Context) error {
 	// Ensure that deploys fail fast rather than waiting forever if child workflow is blocked.
 	ctx = workflow.WithChildOptions(ctx, workflow.ChildWorkflowOptions{
-		WorkflowExecutionTimeout: time.Minute,
+		WorkflowExecutionTimeout: 12*time.Minute,
 	})
 
 	var greeting string
